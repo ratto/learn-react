fc56655311932d0592665f637778c45d12dd7f05
ratto <rattopedro@gmail.com>
2023-07-07 08:37:50 -0300

    getting dummy posts and placing them on screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    posts: []
+  }
+
+  async componentDidMount() {
+    await fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(posts => this.setState({ posts }));
+  }
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <div className="App">
+        {posts.map(p => (
+          <div key={p.id}>
+            <h1>{p.title}</h1>
+            <p>{p.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
